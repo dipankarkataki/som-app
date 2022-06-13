a4f44cd89f240857379f0724da6ced711db786a9
dipankarkataki <dipankarkataki@gmail.com>
2022-06-13 23:02:59 +0530

    dashboard working

--- src/Components/Pages/Dashboard.js
+++ src/Components/Pages/Dashboard.js
@@ -1,3 +1,5 @@
+import {FaUsers, FaRegHandshake, FaAddressBook, FaBoxes} from 'react-icons/fa';
+
 const  Dashboard = () =>{
     return (
         <>
@@ -5,11 +7,51 @@ const  Dashboard = () =>{
                 <p>Dashboard</p>
             </div>
             <div className="content-area">
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <span className="user-icon">
+                                    <FaUsers/>
+                                </span>
+                                This is some text within a card body.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                                <div className="card-body">
+                                    <span className="hand-shake-icon">
+                                        <FaRegHandshake/>
+                                    </span>
+                                    This is some text within a card body.
+                                </div>
+                            </div>
+                        </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                                <div className="card-body">
+                                    <span className="dealer-icon">
+                                        <FaAddressBook/>
+                                    </span>
+                                    This is some text within a card body.
+                                </div>
+                            </div>
+                        </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                                <div className="card-body">
+                                    <span className="product-icon">
+                                        <FaBoxes/>
+                                    </span>
+                                    This is some text within a card body.
+                                </div>
+                            </div>
+                        </div>
+                </div>
                 
             </div>
         </>
-        
-
     );
 }
 
