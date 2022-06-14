a295a77b21f8670e601fdf7de4275502e11fc653
dipankar kataki <dipankar.kataki@ekodus.com>
2022-06-14 14:47:05 +0530

    manage user done

--- src/Components/Pages/ManageUser.js
+++ src/Components/Pages/ManageUser.js
@@ -1,3 +1,5 @@
+import {FaTrashAlt, FaEnvelope, FaPhoneAlt} from 'react-icons/fa';
+
 const ManageUser = () => {
     return (
         <>
@@ -6,6 +8,242 @@ const ManageUser = () => {
                 <button className="btn btn-sm custom-btn">ADD NEW</button>
             </div>
             <div className="content-area">
+                <div className="row">
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3 col-sm-6 col-12">
+                        <div className="card manage-user-card">
+                            <div className="card-body">
+                                <div className="manage-user-content">
+                                    <img src="Images/pic6.jpg" alt="avatar" />
+                                    <FaTrashAlt style={{color:'red'}}/>
+                                </div>
+                                <div className="manage-user-content-details">
+                                    <h5>Jhon Doe</h5>
+                                    <p>
+                                        <FaEnvelope />&nbsp;&nbsp;
+                                        <span>example@xyz.com</span>
+                                    </p>
+                                    <p>
+                                        <FaPhoneAlt />&nbsp;&nbsp;
+                                        <span>7895412014</span>
+                                    </p>
+                                    <p className="referal-code">Referral Code: 1234567</p>
+
+                                    <div className="manage-user-status-details">
+                                        <div className="form-check form-switch">
+                                            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
+                                        </div>
+                                        <button class="btn btn-default btn-sm">VIEW DETAILS</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
