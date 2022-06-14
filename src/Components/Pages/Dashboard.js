55b960e1beffe67df64fa42d3f07c248cbb75e30
dipankarkataki <dipankarkataki@gmail.com>
2022-06-14 09:37:18 +0530

    dashboard 70% completed

--- src/Components/Pages/Dashboard.js
+++ src/Components/Pages/Dashboard.js
@@ -1,4 +1,5 @@
-import {FaUsers, FaRegHandshake, FaAddressBook, FaBoxes} from 'react-icons/fa';
+import {FaUsers, FaRegHandshake, FaAddressBook, FaBoxes, FaEnvelope, FaPhoneAlt} from 'react-icons/fa';
+
 
 const  Dashboard = () =>{
     return (
@@ -66,21 +67,165 @@ const  Dashboard = () =>{
                     <div className="col-md-4 col-sm-12">
                         <div className="card list-user-card">
                             <div className="card-body">
-                                <h5 class="card-title text-center">Last 3 New Users</h5>
+                                <h5 className="card-title text-center">Last 3 New Users</h5>
+                                <div className="user-info-area">
+                                    <div className="user-pic">
+                                        <img src="Images/pic1.jpg" alt="avatar" />
+                                    </div>
+                                    <div className="user-details">
+                                        <h5>Jhon Doe</h5>
+                                        <p>
+                                            <FaEnvelope />&nbsp;&nbsp;
+                                            <span>example@xyz.com</span>
+                                        </p>
+                                        <p>
+                                            <FaPhoneAlt />&nbsp;&nbsp;
+                                            <span>7895412014</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="user-info-area">
+                                    <div className="user-pic">
+                                        <img src="Images/pic1.jpg" alt="avatar" />
+                                    </div>
+                                    <div className="user-details">
+                                        <h5>Jhon Doe</h5>
+                                        <p>
+                                            <FaEnvelope />&nbsp;&nbsp;
+                                            <span>example@xyz.com</span>
+                                        </p>
+                                        <p>
+                                            <FaPhoneAlt />&nbsp;&nbsp;
+                                            <span>7895412014</span>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="user-info-area">
+                                    <div className="user-pic">
+                                        <img src="Images/pic1.jpg" alt="avatar" />
+                                    </div>
+                                    <div className="user-details">
+                                        <h5>Jhon Doe</h5>
+                                        <p>
+                                            <FaEnvelope />&nbsp;&nbsp;
+                                            <span>example@xyz.com</span>
+                                        </p>
+                                        <p>
+                                            <FaPhoneAlt />&nbsp;&nbsp;
+                                            <span>7895412014</span>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-4 col-sm-12">
                         <div className="card list-dealer-card">
                                 <div className="card-body">
-                                    <h5 class="card-title text-center">Last 3 New Dealers</h5>
+                                    <h5 className="card-title text-center">Last 3 New Dealers</h5>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     <div className="col-md-4 col-sm-12">
                         <div className="card list-partner-card">
                                 <div className="card-body">
-                                    <h5 class="card-title text-center">Last 3 New Partners</h5>
+                                    <h5 className="card-title text-center">Last 3 New Partners</h5>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="user-info-area">
+                                        <div className="user-pic">
+                                            <img src="Images/pic1.jpg" alt="avatar" />
+                                        </div>
+                                        <div className="user-details">
+                                            <h5>Jhon Doe</h5>
+                                            <p>
+                                                <FaEnvelope />&nbsp;&nbsp;
+                                                <span>example@xyz.com</span>
+                                            </p>
+                                            <p>
+                                                <FaPhoneAlt />&nbsp;&nbsp;
+                                                <span>7895412014</span>
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
